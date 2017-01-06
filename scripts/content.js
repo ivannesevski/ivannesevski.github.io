e0cedefda671933a40b2f1867d7a5caebc4122ec
Ivan Nesevski <ivan.nesevski@mail.utoronto.ca>
2017-01-06 04:06:27 -0500

    improved arrows

--- scripts/content.js
+++ scripts/content.js
@@ -2,37 +2,40 @@
 
 function showHideContent(event) {
   var selector = event.data.selector;
-  var contentSel = selector + 'content';
-  var btnSel = selector + 'btn';
-  var upSel = selector + 'up';
-  var downSel = selector + 'down';
+  var content = selector + 'content';
+  var btn = selector + 'btn';
+  var lArrow = selector + 'lArrow';
+  var rArrow = selector + 'rArrow';
+    
+  $(content).slideToggle(300);
+  $(lArrow).toggleClass("rotate-left");
+  $(rArrow).toggleClass("rotate-right");
   
-  $(contentSel).slideToggle("fast");
-  $(upSel).toggle();
-  $(downSel).toggle();
-  $(downSel).css("opacity", "1");
-  
-  if ($(upSel).is(':hidden')) {
-    $(btnSel).hover(
+  if (!$(lArrow).hasClass("rotate-left")) {
+    $(btn).hover(
       function() {
-        $(btnSel).css("background", "rgba(208,208,208,0.5)");
-        $(downSel).css("opacity", "1");
+        $(btn).css("background", "rgba(208,208,208,0.5)");
+        $(lArrow).css("opacity", "1");
+        $(rArrow).css("opacity", "1");
       }, 
       function() {
-        $(btnSel).css("background", "rgba(90,50,50,0.2)");
-        $(downSel).css("opacity", "0");
+        $(btn).css("background", "rgba(90,50,50,0.2)");
+        $(lArrow).css("opacity", "0");
+        $(rArrow).css("opacity", "0");
       }
     );
   }
   else {
-    $(btnSel).hover(
+    $(btn).hover(
       function() {
-        $(btnSel).css("background", "rgba(208,208,208,0.5)");
-        $(downSel).css("opacity", "0");
+        $(btn).css("background", "rgba(208,208,208,0.5)");
+        $(lArrow).css("opacity", "1");
+        $(rArrow).css("opacity", "1");
       }, 
       function() {
-        $(btnSel).css("background", "rgba(90,50,50,0.2)");
-        $(downSel).css("opacity", "0");
+        $(btn).css("background", "rgba(90,50,50,0.2)");
+        $(lArrow).css("opacity", "1");
+        $(rArrow).css("opacity", "1");
       }
     );
   }
@@ -42,30 +45,30 @@ function showHideContent(event) {
 $(document).ready(function() {
   $('.about-content').hide();
   $('.contact-content').hide();
-  $('.about-up').hide();
-  $('.contact-up').hide();
-  $('.about-down').show();
-  $('.contact-down').show();
 
   $('.about-btn').hover(
     function() {
       $('.about-btn').css("background", "rgba(208,208,208,0.5)");
-      $('.about-down').css("opacity", "1");
+      $('.about-lArrow').css("opacity", "1");
+      $('.about-rArrow').css("opacity", "1");
     }, 
     function() {
       $('.about-btn').css("background", "rgba(90,50,50,0.2)");
-      $('.about-down').css("opacity", "0");
+      $('.about-lArrow').css("opacity", "0");
+      $('.about-rArrow').css("opacity", "0");
     }
   );
   
   $('.contact-btn').hover(
     function() {
       $('.contact-btn').css("background", "rgba(208,208,208,0.5)");
-      $('.contact-down').css("opacity", "1");
+      $('.contact-lArrow').css("opacity", "1");
+      $('.contact-rArrow').css("opacity", "1");
     }, 
     function() {
       $('.contact-btn').css("background", "rgba(90,50,50,0.2)");
-      $('.contact-down').css("opacity", "0");
+      $('.contact-lArrow').css("opacity", "0");
+      $('.contact-rArrow').css("opacity", "0");
     }
   );
     
